c728c3be006a65057c38e199c049a3ea692f5de2
Felix Nguon <55321150+nguonduc@users.noreply.github.com>
2022-03-10 16:41:10 +0700

    feat: add base components
    - add Heading
    - add Section

---
+++ src/components/base/Heading.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :class="elClass" :is="heading" v-bind="$attrs">
+    <slot>
+      {{ text }}
+    </slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeading',
+  inheritAttrs: false,
+  props: {
+    heading: {
+      type: String,
+      default: 'h1'
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    elClass: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style module></style>
---
+++ src/components/base/Section.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <div class="container mx-auto px-4 sm:px-8 py-4 sm:py-8" v-bind="$attrs">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
