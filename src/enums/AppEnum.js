5d42c482a2af68c9a63cd77f43a38f306dd15d8a
Felix Nguon <55321150+nguonduc@users.noreply.github.com>
2022-03-10 16:41:49 +0700

    chore: add enums

---
+++ src/enums/AppEnum.js
@@ -0,0 +1,29 @@
+const AppEnum = Object.freeze({
+  TITLE: 'COVID19 - WIKI',
+  DESCRIPTION: 'Countries which are most affected by Covid-19',
+  SORTING: [
+    {
+      header: 'TOTAL CONFIRMED',
+      name: 'The most total confirmed cases',
+      slug: 'TotalConfirmed'
+    },
+    {
+      header: 'TOTAL DEATHS',
+      name: 'The highest number of deaths',
+      slug: 'TotalDeaths'
+    },
+    {
+      header: 'TOTAL RECOVERED',
+      name: 'The most total recovered cases',
+      slug: 'TotalRecovered'
+    }
+  ],
+  TABLE: ['Country', 'TOTAL CONFIRMED', 'TOTAL DEATHS', 'TOTAL RECOVERED', 'DATE'],
+  GLOBAL: [
+    ['NewConfirmed', 'TotalConfirmed'],
+    ['NewDeaths', 'TotalDeaths'],
+    ['NewRecovered', 'TotalRecovered']
+  ]
+})
+
+export default AppEnum
